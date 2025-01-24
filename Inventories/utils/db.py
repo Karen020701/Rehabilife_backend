18cd44d4e67ad1c49b70a9b487b48aa5c014e7ca
Alejandra Calvache <amcalvacheo@uce.edu.ec>
2025-01-23 23:43:48 -0500

    add microservice create inventory

---
+++ Inventories/utils/db.py
@@ -0,0 +1,14 @@
+import os
+import pymysql
+from dotenv import load_dotenv
+
+load_dotenv()  # Carga las variables de entorno del archivo .env
+
+def get_db_connection():
+    return pymysql.connect(
+        host=os.getenv("MYSQL_HOST"),
+        user=os.getenv("MYSQL_USER"),
+        password=os.getenv("MYSQL_PASSWORD"),
+        database=os.getenv("MYSQL_DB"),
+        cursorclass=pymysql.cursors.DictCursor
+    )
